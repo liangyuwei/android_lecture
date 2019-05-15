309a47de8edb18b043158bf86915f31a6fe49ae5
liangyuwei <liangyuwei@yahoo.com>
2019-05-15 21:24:07 +0800

    Modified activity_status.xml for assignment C1.(undone)

--- app/src/main/java/cn/zju/id21832004/liangyuwei/StatusActivity.java
+++ app/src/main/java/cn/zju/id21832004/liangyuwei/StatusActivity.java
@@ -2,6 +2,10 @@ package cn.zju.id21832004.liangyuwei;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import cn.iipc.android.tweetlib.SubmitProgram;
 
 public class StatusActivity extends AppCompatActivity {
 
@@ -10,4 +14,29 @@ public class StatusActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hello, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            new SubmitProgram().doSubmit(this, "C1");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
